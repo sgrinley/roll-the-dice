016c10c94697c800e144b4e78f17224e2c117088
sgrinley <shamar808@yahoo.com>
2026-04-16 12:35:23 -0700

    Initial commit

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class Dice {
+
+//  Use method to simulate rolling one die
+    public int roll() {
+
+//  Step 1: Define minimum and maximum values
+        int minValue = 1;
+        int maxValue = 6;
+
+//  Step 2: Generate random number between 1 and 6
+        int randomNumber = (int) (Math.random() * maxValue) + minValue;
+
+//  Step 3: Return the result
+        return randomNumber;
+    }
 }
